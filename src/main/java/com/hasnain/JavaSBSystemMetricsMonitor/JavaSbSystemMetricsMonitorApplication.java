0371d6ba4344d3ec93d60e0693566b6df48c73eb
Hasnain Chaudhry <harif@pgh.yearup.org>
2025-05-24 19:52:31 -0400

    spring init

---
+++ src/main/java/com/hasnain/JavaSBSystemMetricsMonitor/JavaSbSystemMetricsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.hasnain.JavaSBSystemMetricsMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSbSystemMetricsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSbSystemMetricsMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasnain/JavaSBSystemMetricsMonitor/JavaSbSystemMetricsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasnain.JavaSBSystemMetricsMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbSystemMetricsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
