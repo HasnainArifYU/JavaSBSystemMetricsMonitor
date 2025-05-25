a541b67aa0171ba0873de6baa80b5aa2589eb701
Hasnain Chaudhry <harif@pgh.yearup.org>
2025-05-24 23:31:51 -0400

    update

--- src/main/java/com/hasnain/JavaSBSystemMetricsMonitor/controller/MetricController.java
+++ src/main/java/com/hasnain/JavaSBSystemMetricsMonitor/controller/MetricController.java
@@ -1,5 +1,24 @@
 package com.hasnain.JavaSBSystemMetricsMonitor.controller;
+import com.hasnain.JavaSBSystemMetricsMonitor.model.CpuMetric;
+import com.hasnain.JavaSBSystemMetricsMonitor.service.MetricService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/metrics")
 public class MetricController {
-    
+
+    private final MetricService metricService;
+
+    @Autowired
+    public MetricController(MetricService metricService) {
+        this.metricService = metricService;
+    }
+
+
+
+
+
+
 }
