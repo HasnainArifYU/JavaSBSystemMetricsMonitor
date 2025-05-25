6d360a8ef61ba4ee279a52d941a0d5a3e5be3e80
Hasnain Chaudhry <harif@pgh.yearup.org>
2025-05-24 23:33:33 -0400

    create controller class

--- src/main/java/com/hasnain/JavaSBSystemMetricsMonitor/controller/MetricController.java
+++ src/main/java/com/hasnain/JavaSBSystemMetricsMonitor/controller/MetricController.java
@@ -2,6 +2,7 @@ package com.hasnain.JavaSBSystemMetricsMonitor.controller;
 import com.hasnain.JavaSBSystemMetricsMonitor.model.CpuMetric;
 import com.hasnain.JavaSBSystemMetricsMonitor.service.MetricService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,15 @@ public class MetricController {
     public MetricController(MetricService metricService) {
         this.metricService = metricService;
     }
-
-
+    /**
+     * 🧠 Endpoint to get the latest CPU usage metric
+     * URL: GET /api/metrics/cpu
+     * Returns: JSON with usage and timestamp
+     */
+    @GetMapping("/cpu")
+    public CpuMetric getCpuMetric() {
+        return metricService.getCurrentCpuUsage();
+    }
 
 
 
